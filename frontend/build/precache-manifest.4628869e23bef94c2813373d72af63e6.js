6388b5130173271a9664f8ea8e4e485589bedae9
Frederik Østergaard <fkoe90@gmail.com>
2020-11-01 14:25:30 +0100

    cleanup

---
+++ frontend/build/precache-manifest.4628869e23bef94c2813373d72af63e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb83009712a309fe55ee12dd642e8fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622f2b70df4f4e718d1a",
+    "url": "/static/js/2.1789b2de.chunk.js"
+  },
+  {
+    "revision": "17dc83cb8d10dda07d16b360ca246f81",
+    "url": "/static/js/2.1789b2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3408fc938bb9a6e538",
+    "url": "/static/js/main.868f6f27.chunk.js"
+  },
+  {
+    "revision": "e43c7ab41222ffab0579",
+    "url": "/static/js/runtime-main.da1f3d8c.js"
+  },
+  {
+    "revision": "3b72effe91fb99b0eb6de1417d27dfc2",
+    "url": "/static/media/FUNRUNBYFLEMMING-original.3b72effe.svg"
+  },
+  {
+    "revision": "9a8c10bb4a60905f503bc170b5b01a5d",
+    "url": "/static/media/avatar2.9a8c10bb.png"
+  },
+  {
+    "revision": "0fe1513291244f51efd39afc0e478005",
+    "url": "/static/media/register.0fe15132.svg"
+  },
+  {
+    "revision": "77c893a758ae1a547e765f62ffddede0",
+    "url": "/static/media/road1.77c893a7.jpg"
+  },
+  {
+    "revision": "c1bad537246aaaa8a7fba80b21e23be9",
+    "url": "/static/media/runsammen.c1bad537.jpg"
+  },
+  {
+    "revision": "1e7aadac0235b08c984dd2b8343621c8",
+    "url": "/static/media/shoes.1e7aadac.svg"
+  },
+  {
+    "revision": "9930e840cb734fc316b27648f880d52c",
+    "url": "/static/media/user.9930e840.svg"
+  }
+]);
